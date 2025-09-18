f624a82ecef1cfedf501475759e1f2d0d959c775
inggull <inggull@naver.com>
2025-09-19 02:24:50 +0900

    FetchContent 관련 명령어를 추가하고, 프로젝트 이름을 변경한다

--- Dependency.cmake
+++ Dependency.cmake
@@ -1,16 +1,46 @@
+# ExternalProject는 빌드 시에 외부 프로젝트를 클론해와서
+# 자체적으로 구성, 빌드, 설치하는 과정으로 프로젝트와 통합한다
 # ExternalProject 관련 명령어 셋 추가
 include(ExternalProject)
 
+# 클론해올 외부 프로젝트 이름과 여러 옵션들을 설정
 ExternalProject_Add(
     dep-spdlog  # 외부 프로젝트 이름
-    GIT_REPOSITORY "https://github.com/gabime/spdlog.git"
+    GIT_REPOSITORY "https://github.com/gabime/spdlog.git"  # 원격 저장소 주소
     GIT_TAG "v1.x"  # 특정 브랜치나 태그
     GIT_SHALLOW 1  # 특정 브랜치나 태그로부터 최근 1개의 커밋 히스토리만 클론
-    PREFIX ${CMAKE_BINARY_DIR}/install  # 외부 프로젝트를 다운 받아서 빌드하는 경로
-    # 외부 프로젝트의 CMake Configure 인수로 전달
-    CMAKE_ARGS -D CMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/install  # 외부 프로젝트를 설치할 파일 경로
+    PREFIX ${CMAKE_BINARY_DIR}/install  # 외부 프로젝트를 클론해서 빌드하는 경로
+    CMAKE_ARGS  # 외부 프로젝트의 CMake 구성시 인수로 전달
+        -D CMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/install  # 외부 프로젝트를 설치할 파일 경로
 )
 
 # 의존성 리스트 및 라이브러리 파일 리스트 추가
 list(APPEND DEPS dep-spdlog)
+list(APPEND LIBS spdlog$<$<CONFIG:Debug>:d>)  # Build 단계의 CONFIG 인수가 Debug일경우 d를 추가
+
+# 3.11 버전에 추가된 FetchContent는 ExternalProject와 달리 구성 시에 외부 프로젝트를 일단 클론해서
+# 소스 파일 경로를 서브 디렉토리로 추가하는 방식으로 프로젝트와 통합한다
+# FetchContent 관련 명령어 셋 추가
+include(FetchContent)
+
+# 클론해올 외부 프로젝트 이름과 여러 옵션들을 지정
+FetchContent_Declare(
+    spdlog
+    GIT_REPOSITORY https://github.com/gabime/spdlog.git
+    GIT_TAG v1.x
+    GIT_SHALLOW 1
+)
+
+# 구성 시에 외부 프로젝트를 클론하여 소스 파일 경로를 서브 디렉토리로 추가하고
+# 외부 프로젝트 이름을 타겟으로 지정
+FetchContent_MakeAvailable(spdlog)
+
+# FetchContent는 외부 프로젝트의 타겟에 컴파일러 인수를 전달하는 등의 설정이 가능하다
+# ExternalProject는 CMAKE_ARGS에 `-D CMAKE_CXX_FLAGS=/utf-8`로 인수를 전달하거나
+# 소스 파일 경로를 서브 디렉토리로 추가하여 프로젝트를 재정의 하는 방식으로 타겟에 대한 설정이 가능하다
+if (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+    target_compile_options(spdlog PRIVATE /utf-8)
+endif()
+
+list(APPEND DEPS spdlog)
 list(APPEND LIBS spdlog$<$<CONFIG:Debug>:d>)
--- README.md
+++ README.md
@@ -7,7 +7,7 @@ CMake를 공부하며 작성한 간단한 CMake 프로젝트입니다.
 `<CMAKE_SOURCE_DIR>`: `CMakeLists.txt` 파일이 있는 프로젝트의 최상위 경로  
 `<CMAKE_BINARY_DIR>`: 프로젝트가 빌드되는 경로로, 보통은 `build`를 사용
 
-### [Configure]
+### [Configure & Generation]
 
 `cmake <CMAKE_SOURCE_DIR>`  // 현재 폴더에 CMake 구성 파일들을 생성  
 `cmake <CMAKE_SOURCE_DIR> -B <CMAKE_BINARY_DIR>` // `<CMAKE_BINARY_DIR>`에 CMake 구성 파일들을 생성
@@ -16,6 +16,6 @@ CMake를 공부하며 작성한 간단한 CMake 프로젝트입니다.
 * `CMAKE_BUILD_TYPE`을 설정하는 옵션: `-D CMAKE_BUILD_TYPE=<BUILD_TYPE>`  // 생성 단계의 `--config` 옵션보다 우선 순위로 적용
 * 빌드 시스템의 생성 방식을 설정하는 옵션: `-G "<GENERATOR_NAME>"`  // Unix Makefiles
 
-### [Generation]
+### [Build]
 
 `cmake --build <CMAKE_BINARY_DIR>`  // 다중 구성을 지원하는 경우, 빌드 타입을 설정하는 옵션: `--config <BUILD_TYPE>`
